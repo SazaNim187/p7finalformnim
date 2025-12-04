1dac480b8c50b3b227108843adfe359b8553fb0c
Nimrod <nbergado@gbox.ncf.edu.ph>
2025-12-04 19:58:40 +0800

    userservicefixed

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,23 +1,42 @@
 import { Injectable } from '@nestjs/common';
 import { User } from './user.entity';
 import * as bcrypt from 'bcrypt';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Injectable()
 export class UsersService {
+  private file = path.join(__dirname, 'users.json');
   private users: User[] = [];
-  private idCount = 1;
+
+  constructor() {
+    this.load();
+  }
+
+  private load() {
+    if (fs.existsSync(this.file)) {
+      this.users = JSON.parse(fs.readFileSync(this.file, 'utf8'));
+    } else {
+      this.users = [];
+    }
+  }
+
+  private save() {
+    fs.writeFileSync(this.file, JSON.stringify(this.users, null, 2));
+  }
 
   async create(username: string, email: string, password: string) {
     const hashed = await bcrypt.hash(password, 10);
 
     const newUser: User = {
-      id: this.idCount++,
+      id: Date.now(),
       username,
       email,
       password: hashed,
     };
 
     this.users.push(newUser);
+    this.save();
     return newUser;
   }
 
@@ -29,4 +48,3 @@ export class UsersService {
     return this.users.find((u) => u.id === id);
   }
 }
-''
