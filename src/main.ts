7982f0783ab8ddf8b1fdbe8ad9952718d1783036
Nimrod <nbergado@gbox.ncf.edu.ph>
2025-12-05 07:26:34 +0800

    m

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // allow frontend to call backend
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({
+    origin: ['https:nextp7finalformnim.vercel.app'], // or '*' for testing
+  });
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
