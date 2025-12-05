2d9b8f8e85ccad54f8b00208c43358e30d5bb0b2
Nimrod <nbergado@gbox.ncf.edu.ph>
2025-12-05 08:47:49 +0800

    maggana-kana-pls

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
-    origin: ['https:nextp7finalformnim.vercel.app'], // or '*' for testing
+    origin: [
+      'http://localhost:3000',
+      'https://nextp7finalformnim.vercel.app',
+      'https://nextp7finalformnim-git-main-sazanims-projects.vercel.app'
+    ],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
   });
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
